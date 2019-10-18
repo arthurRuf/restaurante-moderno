f48a2f2344314513a041be493a428de3cd64fb33
teteco25 <teteuspereira@hotmail.com>
2019-10-18 02:20:57 -0300

    "adicionei imagem"

--- api/src/models/itemMenu.js
+++ api/src/models/itemMenu.js
@@ -4,6 +4,7 @@ const itemMenuSchema = new mongoose.Schema({
 	name: String,
 	description: String,
 	price: Number,
+	img: String,
 });
 
 module.exports = connection => {
--- theme/screens/RestauranteStack/CadastraItemMenuScreen.js
+++ theme/screens/RestauranteStack/CadastraItemMenuScreen.js
@@ -8,11 +8,13 @@ const CadastraItemMenuScreen = (props) => {
   const [name, setName] = React.useState("");
   const [description, setDescription] = React.useState("");
   const [price, setPrice] = React.useState("");
+  const [img, setImg] = React.useState("");
 
   const save = () => {
     console.log("name", name);
     console.log("description", description);
     console.log("price", price);
+    console.log("img", img);
 
     return fetch(
       "https://kcyst4l620.execute-api.us-east-1.amazonaws.com/dev/itemmenu/create",
@@ -22,6 +24,7 @@ const CadastraItemMenuScreen = (props) => {
           name: name,
           description: description,
           price: price,
+          img: img,
         }),
       }).then((res) => {
       Alert.alert("Item cadastrado com sucesso!");
@@ -52,7 +55,11 @@ const CadastraItemMenuScreen = (props) => {
           value={price}
           onChangeText={setPrice}
         />
-
+ <Text>Img</Text>
+        <Input
+          value={img}
+          onChangeText={setImg}
+        />
         <Button
           center
           onPress={() => {
