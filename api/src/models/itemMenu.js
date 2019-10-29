1f8a5b90e38bf8fad8775e101091c9f169af47cb
teteco25 <teteuspereira@hotmail.com>
2019-10-29 20:55:48 -0300

    "finalizando o commit"

--- api/src/models/itemMenu.js
+++ api/src/models/itemMenu.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
 const itemMenuSchema = new mongoose.Schema({
-<<<<<<< HEAD
 	name: String,
 	description: String,
 	price: Number,
 	img: String,
-=======
-	// _id: mongoose.Schema.Types.ObjectId,
-	image: String,
-	title: String,
-	price: Number,
->>>>>>> 10b89281bb5ff8edeb8518c50f21b79764d8adb1
 });
 
 module.exports = connection => {
