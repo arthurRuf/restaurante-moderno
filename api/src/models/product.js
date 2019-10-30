8753e974aa0ae5c04531643dab5a8f832f365f95
Arthur Ruf Hosang da Costa <arthur.rhc@gmail.com>
2019-10-29 23:40:04 -0300

    commit

--- api/src/models/product.js
+++ api/src/models/product.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-	// _id: mongoose.Schema.Types.ObjectId,
-	title: String,
-	description: String,
-	price: Number,
-	image: String,
+  // _id: mongoose.Schema.Types.ObjectId,
+  title: String,
+  description: String,
+  price: Number,
+  image: String,
 });
 
 module.exports = connection => {
-	return connection.models.product || connection.model('product', productSchema);
+  return connection.models.product || connection.model("product", productSchema);
 };
